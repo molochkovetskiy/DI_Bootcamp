315aed1f57a312c224be04865cc4272750a65f6b
Vitaly <molochkovetskiy@gmail.com>
2023-07-16 14:32:53 +0300

    w2d1 finished exercise xp

---
+++ Week2/Day1/ExerciseXP/ex1-9.py
@@ -0,0 +1,69 @@
+# Exercise 1
+
+# print('Hello world ' * 5)
+
+
+# Exercise 2
+
+# print((99**3)*8)
+
+
+# Exercise 3
+
+# print(5 < 3) # False
+# print(3 == 3) # True
+# print(3 == "3") #False
+# print("3" > 3) # TypeError
+# print("Hello" == "hello") #False
+
+
+# Exercise 4
+
+# computer_brand = 'Asus'
+# print(f'I have a {computer_brand} computer')
+
+
+# Exercise 5
+
+# name = 'Vitaly'
+# age = 28
+# shoe_size = 44
+# info = f"My name is {name} and I'm {28} years old. My shoe size is {shoe_size}."
+# print(info)
+
+
+# Exercise 6
+
+# a = 23
+# b = 14
+
+# if a > b:
+#     print('Hello World')
+
+
+# Exercise 7
+
+# number = int(input('Enter your number: '))
+# if (number % 2) == 0:
+#     print(f'Number {number} is even')
+# else:
+#     print(f'Number {number} is odd')
+
+
+# Exercise 8
+
+# user_name = input('Enter your name: ')
+# my_name = 'Vitaly'
+# if user_name == my_name:
+#     print(f'Wow, we are namesakes! Nice to meet you {user_name}')
+# else:
+#     print(f'Nice to meet you {user_name}. My name is {my_name}')
+
+
+# Exercise 9
+
+height = float(input('Enter your height in inches: '))
+if height * 2.54 >= 145:
+    print("You're tall enough to ride")
+else:
+    print('Sorry, but you need to grom some more to ride')
