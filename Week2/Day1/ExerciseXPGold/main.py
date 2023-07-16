efccf64821fe1d5cdc7e8f97ee38d33059104170
Vitaly <molochkovetskiy@gmail.com>
2023-07-16 15:28:00 +0300

    w2d1 finished exercise xp gold

---
+++ Week2/Day1/ExerciseXPGold/main.py
@@ -0,0 +1,16 @@
+# Exercise 1
+
+# print('Hello world ' * 4, end='')
+# print('I love python ' * 4)
+
+# Exercise 2
+
+user_month = int(input('Enter the number of month: '))
+if user_month in [3, 4, 5]:
+    print("It's Spring")
+elif user_month in [6, 7, 8]:
+    print("It's Summer")
+elif user_month in [9, 10, 11]:
+    print("It's Autumn")
+else:
+    print("It's Winter")
