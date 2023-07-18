f76c90e89ae8d30e2d1a5413c0736f21d8c75cdb
Vitaly <molochkovetskiy@gmail.com>
2023-07-18 21:42:55 +0300

    w2d3 completed timed challenges

---
+++ Week2/Day3/ExerciseXPNinja/timed_challenge.py
@@ -0,0 +1,19 @@
+# Timed Challenge #1
+
+# sentence = input('Enter the sentence:\n').split()
+# reversed_sentence = ' '.join(sentence[::-1])
+# print(reversed_sentence)
+
+
+# Timed Challenge #2
+
+# num = int(input('Enter the number: ')) 
+# divisors_sum = 0
+
+# for i in range(1, num):
+#     if num % i == 0:
+#         divisors_sum += i
+# if divisors_sum == num:
+#     print('True')
+# else:
+#     print('False')
