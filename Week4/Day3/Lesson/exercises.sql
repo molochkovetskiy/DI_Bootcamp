da569145651ffa4b9caef9bb64a2885563677d7f
Vitaly <molochkovetskiy@gmail.com>
2023-08-01 20:22:00 +0300

    w4d3 exercises from the lesson

---
+++ Week4/Day3/Lesson/exercises.sql
@@ -0,0 +1,103 @@
+-- PART 1
+
+-- CREATE SEQUENCE DEPARTMENT_DEPTCODE
+-- START 10
+-- INCREMENT 10;
+
+-- CREATE TABLE DEPARTMENT (
+-- 	DEPTCODE INTEGER DEFAULT nextval('DEPARTMENT_DEPTCODE') PRIMARY KEY,
+--    	DeptName CHAR(30),
+--    	LOCATION VARCHAR(33) UNIQUE
+-- );
+
+-- CREATE TABLE EMPLOYEE (
+-- 	EmpCode INTEGER PRIMARY KEY,
+--    	EmpFName VARCHAR(15) NOT NULL,
+--    	EmpLName VARCHAR(15) NOT NULL,
+--    	Job VARCHAR(45),
+--    	Manager CHAR(4),
+--    	HireDate DATE,
+--    	Salary DECIMAL DEFAULT 0,
+--    	Commission INTEGER,
+--    	Department_code INTEGER REFERENCES DEPARTMENT(DEPTCODE)
+-- );
+
+-- PART 2
+
+-- INSERT INTO DEPARTMENT (DeptName,LOCATION) VALUES 
+-- ('FINANCE', 'EDINBURGH'),
+-- ('SOFTWARE','PADDINGTON'),
+-- ('SALES', 'MAIDSTONE'),
+-- ('MARKETING', 'DARLINGTON'),
+-- ('ADMIN', 'BIRMINGHAM');   
+
+-- INSERT INTO EMPLOYEE (EmpCode,EmpFName,EmpLName,Job,Manager,HireDate,Salary,Commission,Department_code)
+-- VALUES (9369, 'TONY', 'STARK', 'SOFTWARE ENGINEER', 7902, '1980-12-17', 2800,0,20),
+--        (9499, 'TIM', 'ADOLF', 'SALESMAN', 7698, '1981-02-20', 1600, 300,30),    
+--        (9566, 'KIM', 'JARVIS', 'MANAGER', 7839, '1981-04-02', 3570,0,20),
+--        (9654, 'SAM', 'MILES', 'SALESMAN', 7698, '1981-09-28', 1250, 1400, 30),
+--        (9782, 'KEVIN', 'HILL', 'MANAGER', 7839, '1981-06-09', 2940,0,10),
+--        (9788, 'CONNIE', 'SMITH', 'ANALYST', 7566, '1982-12-09', 3000,0,20),
+--        (9839, 'ALFRED', 'KINSLEY', 'PRESIDENT', 7566, '1981-11-17', 5000,0, 10),
+--        (9844, 'PAUL', 'TIMOTHY', 'SALESMAN', 7698, '1981-09-08', 1500,0,30),
+--        (9876, 'JOHN', 'ASGHAR', 'SOFTWARE ENGINEER', 7788, '1983-01-12',3100,0,20),
+--        (9900, 'ROSE', 'SUMMERS', 'TECHNICAL LEAD', 7698, '1981-12-03', 2950,0, 20),
+--        (9902, 'ANDREW', 'FAULKNER', 'ANALYST', 7566, '1981-12-03', 3000,0, 10),
+--        (9934, 'KAREN', 'MATTHEWS', 'SOFTWARE ENGINEER', 7782, '1982-01-23', 3300,0,20),
+--        (9591, 'WENDY', 'SHAWN', 'SALESMAN', 7698, '1981-02-22', 500,0,30),
+--        (9698, 'BELLA', 'SWAN', 'MANAGER', 7839, '1981-05-01', 3420, 0,30),
+--        (9777, 'MADII', 'HIMBURY', 'ANALYST', 7839, '1981-05-01', 2000, 200, NULL),
+--        (9860, 'ATHENA', 'WILSON', 'ANALYST', 7839, '1992-06-21', 7000, 100, 50),
+--        (9861, 'JENNIFER', 'HUETTE', 'ANALYST', 7839, '1996-07-01', 5000, 100, 50);
+
+-- PART 3
+
+-- SELECT COUNT(*)
+-- FROM EMPLOYEE
+-- WHERE Department_code = 10;
+
+-- SELECT COUNT(*)
+-- FROM EMPLOYEE
+-- WHERE Department_code = 10 AND JOB = 'ANALYST';
+
+-- SELECT e.EmpFName, e.EmpLName, e.JOB, d.LOCATION
+-- FROM DEPARTMENT AS d
+-- INNER JOIN EMPLOYEE AS e ON d.DEPTCODE = e.Department_code;
+
+-- SELECT ROUND(AVG(Salary))
+-- FROM EMPLOYEE
+-- WHERE JOB = 'SOFTWARE ENGINEER';
+
+-- SELECT e.EmpFName, e.EmpLName, e.Department_code, d.DeptName, d.LOCATION
+-- FROM DEPARTMENT AS d
+-- INNER JOIN EMPLOYEE AS e ON d.DEPTCODE = e.Department_code
+-- ORDER BY e.EmpFName, d.LOCATION;
+
+-- SELECT EmpFName, Salary + Commission AS TOTAL_SALARY
+-- FROM EMPLOYEE;
+
+-- SELECT MAX(Salary)
+-- FROM EMPLOYEE;
+
+-- SELECT EmpFName, SALARY, DEPARTMENT_CODE
+-- FROM EMPLOYEE;
+
+-- PART 4
+
+-- CREATE TABLE Boss (
+-- boss_id serial PRIMARY KEY,
+-- boss_name varchar(50),
+-- boss_type varchar(50),
+-- dept_number integer REFERENCES DEPARTMENT(DEPTCODE) UNIQUE
+-- );
+
+-- INSERT INTO Boss (boss_name, boss_type, dept_number)
+-- VALUES 
+-- 	('Andrei', 'Nice', (SELECT DEPTCODE FROM DEPARTMENT WHERE DeptName = 'FINANCE')),
+-- 	('Lise', 'Funny', (SELECT DEPTCODE FROM DEPARTMENT WHERE DeptName = 'SALES')),
+-- 	('Megan', 'Friendly', (SELECT DEPTCODE FROM DEPARTMENT WHERE DeptName = 'MARKETING')
+-- 	);
+
+-- SELECT boss_name, boss_type, DeptName
+-- FROM Boss
+-- INNER JOIN DEPARTMENT ON Boss.dept_number = DEPARTMENT.DEPTCODE;
